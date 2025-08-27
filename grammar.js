c5fdac2bd3380939b392792093b84dbf063d446f
wilitp <guilledeipola@gmail.com>
2025-08-27 03:10:36 -0300

    return statement

--- grammar.js
+++ grammar.js
@@ -12,14 +12,19 @@ module.exports = grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => choice( seq("void", $.main)),
+    source_file: $ => choice(seq("void", $.main)),
     main: $ => seq("main", $._args, $._block),
-    _args: $ => seq("(", repeat( "arg"), ")"),
-    _block: $ => seq("{", repeat(seq($.command, ";")) ,"}"),
-    command: $=> (choice($._block, "skip", $.intexp)),
+    _args: _$ => seq("(", repeat("arg"), ")"),
+    _block: $ => seq("{", repeat(seq($._statement, ";")), "}"),
+    _statement: $ => (choice($.return_statement, "skip", $.intexp)),
     intexp: $ => choice((seq("(", $._int_operation, ")"), $._num)),
-    _int_operation: $=> choice (seq($._num, "+", $._num),seq($._num, "/",$._num),
-                                seq($._num, "*", $._num),seq($._num, "-", $._num),),   
-    _num: $ => "num"
+    _int_operation: $ => choice(
+      seq($._num, "+", $._num),
+      seq($._num, "/", $._num),
+      seq($._num, "*", $._num),
+      seq($._num, "-", $._num),
+    ),
+    _num: _$ => "num",
+    return_statement: $ => seq("return", $.intexp)
   }
 });
