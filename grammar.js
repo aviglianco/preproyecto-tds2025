85ad499d8821c2d9bc8223867430648294fe185f
wilitp <guilledeipola@gmail.com>
2025-08-28 18:36:13 -0300

    precedence, asociativity, int constants and parenthesis

--- grammar.js
+++ grammar.js
@@ -15,17 +15,24 @@ module.exports = grammar({
     main: $ => seq("main", $.args, $._block),
     args: _$ => seq("(", repeat(seq("arg", ",")), ")"),
     _block: $ => seq("{", repeat(seq($._statement, ";")), "}"),
-    _statement: $ => (choice($.return_statement, "skip", $.intexp)),
+    _statement: $ => (choice($.return_statement, "skip", $._intexp1)),
 
     // TODO: remove parenthesis by using precedence and asociativity
-    int_operation: $ => choice(
-      seq("(", $.intexp, "+", $.intexp, ")"),
-      seq("(", $.intexp, "/", $.intexp, ")"),
-      seq("(", $.intexp, "*", $.intexp, ")"),
-      seq("(", $.intexp, "-", $.intexp, ")"),
+    _int_operation: $ => choice(
+      $.int_proc,
+      $.int_div,
+      $.int_sum,
+      $.int_sub,
     ),
-    intexp: $ => choice($.int_operation, $.num),
-    num: _$ => "num",
-    return_statement: $ => seq("return", $.intexp)
+
+    int_proc: $ => prec.right(1, seq($._intexp1, "*", $._intexp1)),
+    int_div: $ => prec.right(2, seq($._intexp1, "/", $._intexp1)),
+    int_sum: $ => prec.right(3, seq($._intexp1, "+", $._intexp1)),
+    int_sub: $ => prec.right(4, seq($._intexp1, "-", $._intexp1)),
+
+    _intexp2: $ => choice($._int_operation, $.num),
+    _intexp1: $ => choice($._intexp2, seq("(", $._intexp1, ")")),
+    num: _$ => /\d+/,
+    return_statement: $ => seq("return", $._intexp1)
   }
 });
