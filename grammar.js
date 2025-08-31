91576452fc55b2952997e14e330df7b769e0993b
wilitp <guilledeipola@gmail.com>
2025-08-31 04:18:46 -0300

    constantes booleanas

--- grammar.js
+++ grammar.js
@@ -7,7 +7,7 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-module.exports = grammar({
+export default grammar({
   name: "preprojectlang",
 
   rules: {
@@ -21,7 +21,7 @@ module.exports = grammar({
     _int_type: _$ => "int",
     _type: $ => choice($._int_type, $._bool_type),
     _block: $ => seq("{", repeat(seq($._statement, ";")), "}"),
-    _identifier: _$ => /[a-z][a-z,0-9]*/,
+    identifier: _$ => /[a-z][a-z,0-9]*/,
     _statement: $ => (choice($.return_statement, "skip", $.declaration_statement, $.assignment_statement, $._intexp1)),
 
     declaration_statement: $ => seq($._type, $._identifier),
@@ -36,13 +36,18 @@ module.exports = grammar({
       $.int_sub,
     ),
 
-    int_proc: $ => prec.right(1, seq($._intexp1, "*", $._intexp1)),
-    int_div: $ => prec.right(2, seq($._intexp1, "/", $._intexp1)),
-    int_sum: $ => prec.right(3, seq($._intexp1, "+", $._intexp1)),
-    int_sub: $ => prec.right(4, seq($._intexp1, "-", $._intexp1)),
+    int_proc: $ => prec.right(1, seq($._expression, "*", $._expression)),
+    int_div: $ => prec.right(2, seq($._expression, "/", $._expression)),
+    int_sum: $ => prec.right(3, seq($._expression, "+", $._expression)),
+    int_sub: $ => prec.right(4, seq($._expression, "-", $._expression)),
 
-    _intexp2: $ => choice($._int_operation, $.num, $._identifier),
-    _intexp1: $ => choice($._intexp2, seq("(", $._intexp1, ")")),
+    _exp: $ => choice($._int_operation, $.num, $._bool_const, $._identifier),
+    _expression: $ => choice($._intexp2, seq("(", $._expression, ")")),
+
+    true: _$ => "true",
+    false: _$ => "false",
+    _bool_const: $ => choice($.true, $.false),
+
     num: _$ => /\d+/,
   }
 });
