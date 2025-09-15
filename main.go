09bd861aeaf2c728042a9ad75a2d0542595d64e4
aviglianco <aviglianco22@gmail.com>
2025-09-15 10:50:37 -0300

    Actualizar main para tomar archivos de entrada y actualizar README

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	parserlang "compilador/bindings/go"
 
 	sitter "github.com/tree-sitter/go-tree-sitter"
 )
-
-const testCode = `bool main(){
-		int a;
-		a = 5 + 2;
-		bool b;
-		b = true;
-		int c;
-		c = a + 17;
-		return c;
-	}`
 
 func main() {
 	parser := sitter.NewParser()
@@ -32,7 +24,25 @@ func main() {
 		panic(fmt.Errorf("couldn't configure parser: %w", e))
 	}
 
-	code := []byte(testCode)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: compilador <input.ctds>")
+		os.Exit(1)
+	}
+
+	inputArg := os.Args[1]
+
+	if filepath.Ext(inputArg) != ".ctds" {
+		fmt.Fprintln(os.Stderr, "error: input file must have .ctds extension")
+		os.Exit(1)
+	}
+
+	var code []byte
+	var err error
+	code, err = os.ReadFile(inputArg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading input: %v\n", err)
+		os.Exit(1)
+	}
 
 	// Parse the code
 	tree := parser.Parse(code, nil)
@@ -41,20 +51,14 @@ func main() {
 	// Get the root node
 	root := tree.RootNode()
 
-	// Pretty-print the syntax tree
-	fmt.Println(root.ToSexp())
-
-	// Build AST
-	program, err := buildProgram(root, code)
-	if err != nil {
-		fmt.Println("build error:", err)
-		return
+	// Pretty-print the syntax tree and write to .sint file
+	output := []byte(root.ToSexp())
+	base := inputArg[:len(inputArg)-len(filepath.Ext(inputArg))]
+	outputPath := base + ".sint"
+	if err := os.WriteFile(outputPath, output, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "error writing output: %v\n", err)
+		os.Exit(1)
 	}
 
-	// Pretty-print the resulting AST
-	fmt.Println(printAST(program))
-
-	// Generate assembly
-	asm := generateAssembly(program)
-	fmt.Println(asm)
+	fmt.Println("Output written to:", outputPath)
 }
